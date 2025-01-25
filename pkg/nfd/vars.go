b7aa1ea4ff95f457d9f146f26fed73afe52ca64d
akaabyia <akaabyia@redhat.com>
2025-01-25 18:52:57 +0200

    1. seperated global variables from nfd vars 2. fixed nfd consts names 3.fixed nfd var bug - create instance per package not global nfd

--- pkg/nfd/vars.go
+++ pkg/nfd/vars.go
@@ -1,9 +1,21 @@
 package nfd
 
-var (
-	NfdCustomCatalogsourceIndexImage = UndefinedValue
-	CreateNFDCustomCatalogsource     = false
-	NfdCustomCatalogSource           = UndefinedValue
-	NfdCatalogSource                 = UndefinedValue
-	NfdCleanupAfterInstall           = false
-)
+import . "github.com/rh-ecosystem-edge/nvidia-ci/pkg/global"
+
+type Config struct {
+	CustomCatalogSourceIndexImage string
+	CreateCustomCatalogsource     bool
+	CustomCatalogSource           string
+	CatalogSource                 string
+	CleanupAfterInstall           bool
+}
+
+func NewConfig() *Config {
+	return &Config{
+		CustomCatalogSourceIndexImage: UndefinedValue, // Use the constant as the default
+		CreateCustomCatalogsource:     false,          // Default value as specified
+		CustomCatalogSource:           UndefinedValue, // Use the constant as the default
+		CatalogSource:                 UndefinedValue, // Use the constant as the default
+		CleanupAfterInstall:           false,          // Default value as specified
+	}
+}
