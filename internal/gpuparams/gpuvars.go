014c17c94953837f44875dae962c2fa50323c7ec
Vitaliy Emporopulo <vemporop@redhat.com>
2026-02-06 21:23:21 +0200

    Fix formatting using gofmt

--- internal/gpuparams/gpuvars.go
+++ internal/gpuparams/gpuvars.go
@@ -2,6 +2,6 @@ package gpuparams
 
 var (
 	// Labels represents the range of labels that can be used for test cases selection.
-	Labels = []string{"nvidia-ci", Label}
+	Labels    = []string{"nvidia-ci", Label}
 	MigLabels = []string{"mig", Label}
 )
