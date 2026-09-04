c5ae38eb51d6ded8f2ae6758b212e597346738ed
Jose Castillo Lema <josecastillolema@gmail.com>
2026-09-04 11:32:49 +0200

    Add a new artifact to save driver branch version
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- tests/nvidiagpu/deploygpu_test.go
+++ tests/nvidiagpu/deploygpu_test.go
@@ -680,6 +680,11 @@ var _ = Describe("GPU", Ordered, Label(tsparams.LabelSuite), func() {
 				Expect(err).ToNot(HaveOccurred(), "Failed to discover precompiled driver version: %v", err)
 				glog.V(gpuparams.GpuLogLevel).Infof("Discovered precompiled driver version: %s", driverVersion)
 
+				if err := inittools.GeneralConfig.WriteReport(
+					DriverBranchVersionsFile, []byte(driverVersion+"\n")); err != nil {
+					glog.Error("Error writing driver branch versions file: ", err)
+				}
+
 				precompiledOps := []map[string]interface{}{
 					{"op": "add", "path": "/spec/driver/usePrecompiled", "value": true},
 					{"op": "add", "path": "/spec/driver/repository", "value": nvidiagpu.PrecompiledDriverRepoField},
