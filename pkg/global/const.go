2e8e2bb37b8033aacf29b1905e0a04678bb74241
Jose Castillo Lema <josecastillolema@gmail.com>
2026-09-04 11:32:49 +0200

    Add a new artifact to save driver branch version
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>

--- pkg/global/const.go
+++ pkg/global/const.go
@@ -2,7 +2,8 @@ package global
 
 const (
 	// not related to NFD but common consts between gpu and nno
-	UndefinedValue       = "undefined"
-	OperatorVersionFile  = "operator.version"
-	OpenShiftVersionFile = "ocp.version"
+	UndefinedValue           = "undefined"
+	OperatorVersionFile      = "operator.version"
+	OpenShiftVersionFile     = "ocp.version"
+	DriverBranchVersionsFile = "driver.branches"
 )
